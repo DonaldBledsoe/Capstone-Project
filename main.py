e4d7fe219735e3f14a3677413493b57620736024
DonaldBledsoe <donald.bledsoe77@gmail.com>
2022-01-06 10:28:46 -0600

    Start of capstone, run 1

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 
-
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.io.gcp.internal.clients import bigquery
 
 
+table1 = "york-cdf-start.bigquerypython.bqtable1"
+table2 = "york-cdf-start.bigquerypython.bqtable4"
 
-topic_id = "projects/york-cdf-start/topics/dataflow-order-stock-update"     # topic for WriteToPubSub
 
 if __name__ == '__main__':
+    pipeline_options = PipelineOptions(
+        temp_location='gs://db7_temp_loc/',
+        # staging_location='gs://db7_temp_loc/',
+        # save_main_session=True,
+        # region='us-central1',
+        # runner='DataflowRunner',
+        # job_name='dons-job',
+        # project='york-cdf-start',
+        # dataset='bigquerypython'
+    )
 
     # Schema definition for all of the payment order history tables
     table_schema = {
         'fields': [
             {'name': 'order_id', 'type': 'INTEGER', 'mode': 'nullable'},
-            {'name': 'order_address', 'type': 'RECORD', 'mode': 'Repeated',
-                'fields': [
-                    {'name': 'order_building_num', 'type': 'STRING', 'mode': 'nullable'},
-                    {'name': 'order_street_name', 'type': 'STRING', 'mode': 'nullable'},
-                    {'name': 'order_city', 'type': 'STRING', 'mode': 'nullable'},
-                    {'name': 'order_state_code', 'type': 'STRING', 'mode': 'nullable'},
-                    {'name': 'order_zip_code', 'type': 'STRING', 'mode': 'nullable'},
-            ],
-            },
+            {'name': 'last_name', 'type': 'STRING', 'mode': 'nullable'}
 
-            {'name': 'customer_first_name', 'type': 'STRING', 'mode': 'nullable'},
-            {'name': 'customer_last_name', 'type': 'STRING', 'mode': 'nullable'},
-            {'name': 'customer_ip', 'type': 'STRING', 'mode': 'nullable'},
-            {'name': 'cost_total', 'type': 'Float', 'mode': 'nullable'},
         ]
     }
 
@@ -35,37 +33,28 @@ if __name__ == '__main__':
     table_usd_spec = bigquery.TableReference(
         projectId='york-cdf-start',
         datasetId='d_bledsoe_dataflow1',
-        tableId='usd_order_payment_history'
+        tableId='test_table1'
     )
 
-    table_gbp_spec = bigquery.TableReference(
-        projectId='york-cdf-start',
-        datasetId='d_bledsoe_dataflow1',
-        tableId='gbp_order_payment_history'
-    )
+    # table_gbp_spec = bigquery.TableReference(
+    #     projectId='york-cdf-start',
+    #     datasetId='d_bledsoe_dataflow1',
+    #     tableId='gbp_order_payment_history'
+    # )
 
-    table_eur_spec = bigquery.TableReference(
-        projectId='york-cdf-start',
-        datasetId='d_bledsoe_dataflow1',
-        tableId='eur_order_payment_history'
-    )
-
-    with beam.Pipeline(options=PipelineOptions()) as pipeline:
-        orders = pipeline | beam.io.ReadFromBigQuery()
+    with beam.Pipeline(options=pipeline_options) as pipeline:
+        data3 = (
+                pipeline | 'Query tables' >> beam.io.ReadFromBigQuery(
+                    query='SELECT table1.name, table2.last_name FROM york-cdf-start.bigquerypython.bqtable1 as table1 '
+                           'join york-cdf-start.bigquerypython.bqtable4 as table2 on table1.order_id = table2.order_id', project="york-cdf-start",use_standard_sql=True)
+        )
 
 
-        # separating the orders by currency
-        usd_split = total_cost | beam.Filter(usd)          #USD orders
-
-        gbp_split = total_cost | beam.Filter(gbp)          # GBP orders
-
-        eur_split = total_cost | beam.Filter(eur)          # Eur orders
-
-
-
+        # data = pipeline | "Read Table 1" >> beam.io.ReadFromBigQuery(table=table1) | beam.Map(print)
+        # data2 = pipeline | "Read Table 2" >> beam.io.ReadFromBigQuery(table=table2) | beam.Map(print)
 
         # Outputting the tables to BigQuery.
-        USD_Orders | "Write1" >> beam.io.WriteToBigQuery(
+        data3 | "Write1" >> beam.io.WriteToBigQuery(
             table_usd_spec,
             schema=table_schema,
             create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
@@ -73,19 +62,19 @@ if __name__ == '__main__':
             method="STREAMING_INSERTS"
         )
 
-        GBP_Orders | "Write2" >> beam.io.WriteToBigQuery(
-            table_gbp_spec,
-            schema=table_schema,
-            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
-            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
-            method="STREAMING_INSERTS"
-
-        )
-
-        EUR_Orders | "Write3" >> beam.io.WriteToBigQuery(
-            table_eur_spec,
-            schema=table_schema,
-            create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
-            write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
-            method="STREAMING_INSERTS"
-        )
+        # GBP_Orders | "Write2" >> beam.io.WriteToBigQuery(
+        #     table_gbp_spec,
+        #     schema=table_schema,
+        #     create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+        #     write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+        #     method="STREAMING_INSERTS"
+        #
+        # )
+        #
+        # EUR_Orders | "Write3" >> beam.io.WriteToBigQuery(
+        #     table_eur_spec,
+        #     schema=table_schema,
+        #     create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+        #     write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+        #     method="STREAMING_INSERTS"
+        # )
